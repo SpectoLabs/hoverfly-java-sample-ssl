5cb8d8af0668c31ef839145918dd37d33fa0e63f
Tommy Situ <tommy.situ@specto.io>
2017-02-21 21:09:58 +0000

    Initial commit

---
+++ src/main/java/io/specto/HoverflyJavaSampleSslApplication.java
@@ -0,0 +1,12 @@
+package io.specto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoverflyJavaSampleSslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoverflyJavaSampleSslApplication.class, args);
+	}
+}
